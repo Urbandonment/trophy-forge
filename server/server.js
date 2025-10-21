158b64681d2b6f90b849e954508ade0ef2971afb
Urbandonment <tu.nguyenminh2@hotmail.com>
2025-10-21 15:26:42 +0700

    Change app.get for static files

--- server/server.js
+++ server/server.js
@@ -228,7 +228,7 @@ app.listen(PORT, () => {
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/dist")));
 
-  app.get("*", (req, res) => {
+  app.get("/*splat", (req, res) => {
     return res.sendFile(path.join(__dirname, "../client/dist/index.html"));
   });
 }
