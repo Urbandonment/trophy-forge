53b00b7f5b8e7e88b41470773bc782df2591ac09
Urbandonment <66505497+Urbandonment@users.noreply.github.com>
2025-11-06 23:33:32 +0700

    Remove Analytics

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -26,8 +26,6 @@ import SilverTC from './assets/trophy-card-silver-trophy.png';
 import GoldTC from './assets/trophy-card-gold-trophy.png';
 import PlatinumTC from './assets/trophy-card-platinum-trophy.png';
 
-import { Analytics } from '@vercel/analytics/react';
-
 const isDevelopment = process.env.NODE_ENV === 'development';
 // If in development, point to the local server/port (e.g., 3001).
 // If in production (Vercel), use an empty string so the path becomes relative (e.g., "/api/psn-profile/...")
@@ -660,7 +658,6 @@ function Home() {
           </div>
         </div>
       </div>
-      <Analytics />
     </div>
   );
 }
