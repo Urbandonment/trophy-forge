d1d316db50121edf92ae0046a6288b13816448b1
Urbandonment <tu.nguyenminh2@hotmail.com>
2025-11-10 15:42:28 +0700

    Added Google Analytics

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,11 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Home from './Home.jsx'
-import { Analytics } from '@vercel/analytics/react';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Analytics />
     <Home />
   </StrictMode>,
 )
